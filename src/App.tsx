6a4351661a9224e8f2cb13fa896cdcdcf8879914
kyleh <hi@kyleh.io>
2022-08-24 10:16:51 -0400

    chore: set up base app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react';
+
+const App: FunctionComponent = () => {
+    return (
+        <div className="app">
+            hi
+        </div>
+    );
+}
+
+export default App;
