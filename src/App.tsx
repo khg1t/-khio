49371fb9e58ef860e1ede3df7bc334ee62cf4038
kyleh <hi+git@kyleh.io>
2022-08-24 13:16:07 -0400

    chore: set base linting + formatting rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import React, { FunctionComponent } from 'react';
 
 export const App: FunctionComponent = () => {
-    return (
-        <div className="app">
-            hi
-        </div>
-    );
+    return <div className="app">khio</div>;
 };
